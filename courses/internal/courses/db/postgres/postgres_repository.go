f99869e4d1baf53dd361d35b54043f27aa999060
priort <priordtom@gmail.com>
2022-04-09 16:56:57 +0100

    added pgxpool Go postgres lib

--- courses/internal/courses/db/postgres/postgres_repository.go
+++ courses/internal/courses/db/postgres/postgres_repository.go
@@ -1,1 +1,38 @@
 package postgres
+
+import (
+	"context"
+	"log"
+	"os"
+	"swapknowhow/courses/internal/courses"
+
+	"github.com/jackc/pgx/v4/pgxpool"
+)
+
+type PostgresCoursesRepository struct {
+	dbPool *pgxpool.Pool
+	Close  func()
+}
+
+func NewPostgresCoursesRepository() *PostgresCoursesRepository {
+	dbPool, err := pgxpool.Connect(context.Background(), "postgresql://localhost:5432/coursesdb")
+
+	if err != nil {
+		log.Fatal("db connection could not be established")
+		os.Exit(1)
+	}
+
+	return &PostgresCoursesRepository{
+		dbPool: dbPool,
+		Close:  func() { dbPool.Close() },
+	}
+
+}
+
+func (repo *PostgresCoursesRepository) GetCourses() []courses.Course {
+	return make([]courses.Course, 0, 10)
+}
+
+func (repo *PostgresCoursesRepository) CreateCourse(course courses.Course) {
+	// repo.dbPool.Exec(context.Background(), "INSERT INTO ")
+}
